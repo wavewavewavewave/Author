a8cdbb924a1c16aef388618c46995b075321e7f1
wavewavewavewave <nikitashkrabov@gmail.com>
2024-04-04 16:10:30 +0300

    create format_printing page

--- src/app/pages/services/large-format-printing/page.tsx
+++ src/app/pages/services/large-format-printing/page.tsx
@@ -1,20 +1,20 @@
 'use client'
 import React from 'react';
+import styles from './formatPrinting.module.css'
+import cat from '../../../assets/Cats_contact/cat3.svg'
 import Header from "../../../components/Header/Header";
-import styles from './lightBoxes.module.css'
-import arrow from '../../../assets/Vector.svg'
-import cat from '../../../assets/Cats_contact/cat1.svg'
+import {format_printing_arr} from "../../../constants/Constants";
 import Image from "next/image";
-import {light_boxes_service_arr} from "../../../constants/Constants";
+import arrow from "../../../assets/Vector.svg";
 
-const LightBoxes = () => {
+const FormatPrinting = () => {
     return (
         <div className={styles.container_page}>
             <Header services={true}/>
             <div className={styles.container_boxes}>
-                <h1>Световые короба</h1>
+                <h1>Широкоформатная печать</h1>
                 <div className={styles.boxes_block}>
-                    {light_boxes_service_arr.map((box) => (
+                    {format_printing_arr.map((box) => (
                         <div key={box.id} style={{position: 'relative'}}>
                             <div className={styles.box}>
                                 <span className={styles.text}>{box.value}</span>
@@ -22,11 +22,13 @@ const LightBoxes = () => {
                             </div>
                         </div>
                     ))}
-                    <Image src={cat} alt={'Кот контакт'} className={styles.image_cat}/>
+                    <button className={styles.no_background}>
+                        <Image src={cat} alt={'Кот контакт'} className={styles.image_cat}/>
+                    </button>
                 </div>
             </div>
         </div>
     );
 };
 
-export default LightBoxes;
+export default FormatPrinting;
--- src/app/pages/services/signs/page.tsx
+++ src/app/pages/services/signs/page.tsx
@@ -3,43 +3,13 @@ import React from 'react';
 import styles from './signs.module.css'
 import arrow from '../../../assets/Vector.svg'
 import Header from "../../../components/Header/Header";
-import cat from '../../../assets/cat1.svg'
+import cat from '../../../assets/Cats_contact/cat1.svg'
 import Image from "next/image";
-import test from '../../../assets/img_viber_2024-02-12_11-40-19-814.jpg'
+import {signs_array} from "../../../constants/Constants";
 
 
 const Signs = () => {
-    const signs_array = [
-        {id: 0, value: 'Неоновые вывески'},
-        {id: 1, value: 'Светодиодные вывески'},
-        {id: 2, value: 'Фасадные вывески'},
-        {id: 3, value: 'Обьемные вывески'},
-        {id: 4, value: 'Композитные вывески'},
-        {id: 5, value: 'Деревянные вывески'},
-        {id: 6, value: 'Разрешение на вывеску'},
-        {id: 7, value: 'Вывески Режим работы'},
-        {id: 8, value: 'Вывеска для кафе'},
-        {id: 9, value: 'Вывеска для бара'},
-        {id: 10, value: 'Вывеска для салона красоты'},
-        {id: 11, value: 'Вывески для парикмахерской'},
-        {id: 12, value: 'Вывески для барбершопа'},
-        {id: 13, value: 'Вывески для фастфуда'},
-        {id: 14, value: 'Вывески для аптеки'},
-        {id: 15, value: 'Вывески для ателье'},
-        {id: 16, value: 'Вывески для фитнес клуба'},
-        {id: 17, value: 'Вывески для ломбарда'},
-        {id: 18, value: 'Вывески для фитнес клуба'},
-        {id: 19, value: 'Вывески для гостиницы'},
-        {id: 20, value: 'Вывески для магазина'},
-        {id: 21, value: 'Вывески для автомагазина'},
-        {id: 22, value: 'Вывески для мебельноых магазинов'},
-        {id: 23, value: 'Вывески для зоомагазина'},
-        {id: 24, value: 'Вывески для магазина женской одежды'},
-        {id: 25, value: 'Вывески для цветочных магазинов'},
-        {id: 26, value: 'Вывески для шиномонтажа'},
-        {id: 27, value: 'Вывески для автосервиса'},
-        {id: 28, value: 'Аптечный крест'},
-    ]
+
     return (
         <div className={styles.container_page}>
             <Header services={true}/>
